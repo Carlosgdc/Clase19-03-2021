d3568fbd1984973444032af432627778c1e80694
Carlosgdc <cdiazca@unal.edu.co>
2021-04-13 07:32:51 -0500

    En este programa entenderemos que es una función recursiva

--- Clase19-03-2021/main.cpp
+++ Clase19-03-2021/main.cpp
@@ -4,17 +4,30 @@
 //
 //  Created by Countyfoot on 4/12/21.
 //
-// En este programa estudiaremos que es y como se utiliza una función lambda.
+// En este programa entenderemos que es una función recursiva
 
-// requires c++11: -std=c++11
+// Una función recursiva es que al momento de ejecutarse volvera a nombrarse dicha función al momento de retorna algun valor, esto genera que al momento de realizar sucesiones de numeros y/0 operaciones tenemos que es muy sencilla la realización de nuestra función.
 #include <iostream>
-// La función lambda es una función muy corta que para evitar escribirla como una función principal se puede escribir en una declaración como un objeto.
-// [](){}; -> [capture](argumentos){instrucciones;};
+
+int factorial_loop(int a);
+int factorial_recursive(int a);
 
 int main(int argc, char *argv[]) {
-    auto flambda = [](int a, int b) { return a < b; }; // La función auto nos permite que el programa al ser ejecutado deduzca por si mismo que nos estamos refiriendo a una función ya que al hacerlo por el metodo tradicional deberiamos escribir un tipo de función muy complicado.
-    bool flag = flambda(3, 4);
-    std::cout << flag << "\n";
-    std::cout << flambda(4, 3) << "\n";
+
+    std::cout << factorial_loop(5) << std::endl;
+    std::cout << factorial_recursive(5) << std::endl;
+    
+    return 0;
+}
+
+int factorial_loop(int a)
+{
+    //Aca escribimos nuestra función realizada de la manera cotidiana
+    return 0;
+}
+
+int factorial_recursive(int a)
+{
+   //Aca escribimos nuestra función utilizando la función de manera recursiva
     return 0;
 }
